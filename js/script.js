4af26db921d6a51045334983325025fcfb22aa9c
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-11-20 15:57:15 +0100

    Started to add new kind of adding cards with Javascript instead of hardcoding html

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function turnCard(){
 }
 
 memoryCards.forEach(memoryCard => memoryCard.addEventListener('click', turnCard));
+
+
+
+
+// Lägg till array och fixa i html istället för hårdkodning
+
+//array logic
+// let hardGame = cards.concat(cards);
+// hardGame.sort(() => 0.5 - Math.random());
+//
+//
+// hardGame.forEach(value =>{
+//  const card = document.createElement(‘div’);
+//
+//  card.classList.add(‘img’);
+//  card.dataset.name = value.name;
+//  card.style.backgroundImage = `url(${value.img})`;
+//  cardbox.appendChild(card);
+// });
